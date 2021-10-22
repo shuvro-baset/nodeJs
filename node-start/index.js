ec0f1585f4bc01c8011851a80569b27875f95bf4
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-22 23:42:37 +0600

    get

--- node-start/index.js
+++ node-start/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000 // this will come from env file when we added environment setup.
 
+// get method. using default url.
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
