0b9382bd24713cbf2dc248d72c4c4bd4f1558fb3
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-23 12:22:22 +0600

    user

--- node-start/index.js
+++ node-start/index.js
@@ -6,7 +6,14 @@ const port = process.env.PORT || 3000 // this will come from env file when we ad
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+
+
+// get users using '/user' url
+app.get('/users', (req, res) => {
+  res.send('Here will come all users')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
