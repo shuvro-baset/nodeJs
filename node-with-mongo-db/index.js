b1e113b582b934717a917b9a75b93d279f174514
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-24 13:20:16 +0600

    update done

--- node-with-mongo-db/index.js
+++ node-with-mongo-db/index.js
@@ -34,6 +34,31 @@ async function run() {
         console.log('added user', result);
         res.json(result);
   });
+
+      app.get('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const user = await usersCollection.findOne(query);
+        // console.log('load user with id: ', id);
+        res.send(user);
+    });
+
+      //UPDATE API
+      app.put('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        const updatedUser = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                name: updatedUser.name,
+                email: updatedUser.email
+            },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc, options)
+        console.log('updating', id)
+        res.json(result)
+    })
       // DELETE API
       app.delete('/users/:id', async (req, res) => {
         const id = req.params.id;
